5ea33ee2f46143713b04a479fe091df0aca16dd2
kitelife <youngsterxyf@qq.com>
2025-09-20 00:36:37 +0800

    初步完成

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,36 +1,35 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
+use std::path::Path;
 use std::process::Command;
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn fix_video(input: &str, output: &str) -> String {
+    let input_path = Path::new(input).to_str().unwrap();
+    let output_path = Path::new(output).to_str().unwrap();
+    let result = Command::new("ffmpeg")
+        .args(["-y", "-i", input_path, "-c", "copy", output_path])
+        .output().unwrap();
+    let exit_code = result.status.code().unwrap();
+    if exit_code != 0 {
+        let msg = String::from_utf8_lossy(&result.stderr).to_string();
+        println!("failure: convert {} to {}, err: {}", input, output, msg);
+    } else {
+        println!("success: convert {} to {}", input, output);
+    }
+    if exit_code != 0 {
+        return "failure".to_string();
+    }
+    "success".to_string()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![fix_video])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn fix_video(input: &str, output: &str) -> String {
-    let result = Command::new("ffmpeg")
-        .arg(format!("-i '{}' -c copy '{}'", input, output))
-        .output();
-    let msg = match result {
-        Ok(output) => {
-            if output.stdout.len() > 0 {
-                return String::from_utf8_lossy(&output.stdout).to_string();
-            }
-            return String::from_utf8_lossy(&output.stderr).to_string();
-        },
-        Err(err) => err.to_string(),
-    };
-    println!("convert {} to {}, msg: {}", input, output, msg);
-    msg.to_string()
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import {useState} from "react";
 import {invoke} from "@tauri-apps/api/core";
+import { open } from '@tauri-apps/plugin-dialog';
+import { platform } from '@tauri-apps/plugin-os';
 
 import {VideoCameraOutlined} from '@ant-design/icons';
 
 import {Layout, Menu, theme} from 'antd';
-import {Button, Result, message, Input, Form} from 'antd';
+import {Button, Result, message, Input, Progress} from 'antd';
 
 import "./App.css";
 
 const {Sider, Content} = Layout;
+const { TextArea } = Input;
 
-type FieldType = {
-  input_video?: string;
-};
+const os = platform();
+
+function FixProgress(props: {hidden: any, percent: any}) {
+  if (props.hidden) {
+    return <p/>
+  }
+  return <Progress percent={props.percent} type="circle" />
+}
 
 function FixResult(props: { hidden: any; status: any; }) {
   if (props.hidden) {
@@ -25,51 +33,80 @@ function FixResult(props: { hidden: any; status: any; }) {
 }
 
 function FixVideoForm() {
+  const [progressHidden, setProgressHidden] = useState(true);
+  const [progressPercent, setProgressPercent] = useState(0);
   const [resultHidden, setResultHidden] = useState(true);
   const [fixStatus, setFixStatus] = useState(false);
-  const [videoFilePath, setVideoFilePath] = useState("");
+  const [videos, setVideos] = useState('');
   const [messageApi, contextHolder] = message.useMessage();
 
-  const onFileChange = (e) => {
-    console.log(e);
-    setVideoFilePath(e.target.value);
-  };
   const onFixClick = async () => {
-    if (videoFilePath == undefined || videoFilePath.length == 0) {
+    if (videos == undefined || videos.length == 0) {
       messageApi.open({
         type: 'warning',
         content: '请先选择问题视频文件',
       });
       return;
     }
-    await invoke("fix_video", {input: videoFilePath, output: "x.mp4"});
+    setProgressHidden(false);
+    let videoList = videos.split('\n');
+    let progressStep = 100 / videoList.length;
+    for (const ele of videoList) {
+      let last_slash_idx = ele.lastIndexOf(os === "windows" ? '\\' : '/');
+      let video_file_name = ele.substring(last_slash_idx+1, ele.length);
+      let fixed_video_file_name = "修复后_" + video_file_name;
+      let output = ele.substring(0, last_slash_idx+1) + fixed_video_file_name;
+      let status = await invoke("fix_video", {input: ele, output: output});
+      if (status !== "success") {
+        messageApi.open({
+          type: 'warning',
+          content: "修复失败：" + ele,
+        });
+      }
+      setProgressPercent((prePercent) => {
+        let newPercent = prePercent + progressStep;
+        if (newPercent > 100) {
+          newPercent = 100;
+        }
+        return newPercent;
+      });
+    }
+    setProgressHidden(true);
     setFixStatus(true);
     setResultHidden(false);
+  };
+  const onSelectVideoList = async () => {
+    const files = await open({
+      multiple: true,
+      directory: false,
+    });
+    setFixStatus(false);
+    setResultHidden(true);
+    setProgressHidden(true);
+    if (files == null) {
+      messageApi.open({
+        type: 'warning',
+        content: '请选择问题视频文件',
+      });
+      return;
+    }
+    setVideos(files.join('\n'));
   };
 
   return (
     <>
       {contextHolder}
       <main className="container">
-        <Form
-          name="basic"
-          labelCol={{span: 8}}
-          wrapperCol={{span: 16}}
-          style={{maxWidth: 600}}
-          initialValues={{remember: true}}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="问题视频文件"
-            name="input_video"
-            rules={[{required: true, message: '请选择问题视频文件'}]}
-          >
-            <Input type="file" onChange={onFileChange}/>
-          </Form.Item>
-          <Form.Item label={null}>
-            <Button type="primary" htmlType="submit" onClick={onFixClick}>修复</Button>
-          </Form.Item>
-        </Form>
+        <TextArea
+          value={videos}
+          placeholder="点击选择问题视频"
+          autoSize={{ minRows: 1, maxRows: 20 }}
+          onClick={onSelectVideoList}
+        />
+          <Button type="primary" size={"large"}
+                  style={{marginTop: '20px'}}
+                  onClick={onFixClick}>修复</Button>
+        <FixProgress hidden={progressHidden} percent={progressPercent}/>
         <FixResult hidden={resultHidden} status={fixStatus}/>
       </main>
     </>
